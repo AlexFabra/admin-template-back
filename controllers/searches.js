12430e0b55ee164fcc3be43da65ae29ad5aae713
Alejandro <alfaseart@gmail.com>
2022-07-17 16:54:16 +0200

    find by table implemented

--- controllers/searches.js
+++ controllers/searches.js
@@ -1,16 +1,68 @@
 const { response } = require('express');
+const User = require('../models/user');
+const Hospital = require('../models/hospital');
+const Doctor = require('../models/doctor');
 
 const find = async (req, res = response) => {
 
     const searchParam = req.params.by;
+    //regular expression 'i' for vague search
+    const regex = new RegExp(searchParam, 'i');
+
+    const [users, hospitals, doctors] = await Promise.all([
+        User.find({ name: regex }),
+        Hospital.find({ name: regex }),
+        Doctor.find({ name: regex })
+    ])
 
     res.json({
         ok: true,
         found: 'found',
-        searchParam
+        searchParam,
+        users,
+        hospitals,
+        doctors
     })
 }
 
+const findInCollection = async (req, res = response) => {
+
+    const searchTableParam = req.params.table;
+
+    const searchParam = req.params.by;
+    //regular expression 'i' for vague search
+    const regex = new RegExp(searchParam, 'i');
+
+    let data = [];
+
+    switch (searchTableParam) {
+        case 'doctors':
+            data = await Doctor.find({ name: regex })
+            .populate('user','name img')
+            .populate('hospital','name img')
+            break;
+        case 'hospitals':
+            data = await Hospital.find({ name: regex })
+            .populate('user','name img')
+            break;
+        case 'users':
+            data = await User.find({ name: regex })
+            break;
+        default:
+            return res.status(400).json({
+                ok: false,
+                msg: 'error'
+            });
+    }
+
+    res.json({
+        ok: true,
+        results: data
+    });
+
+}
+
 module.exports = {
-    find
+    find,
+    findInCollection
 }
--- routes/searches.js
+++ routes/searches.js
@@ -1,8 +1,10 @@
+// api/search
+
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
-const { find } = require('../controllers/searches');
+const { find,findInCollection } = require('../controllers/searches');
 
 const router = Router();
 
@@ -10,6 +12,11 @@ router.get(
     '/:by',
     [validateJWT],
     find
+);
+router.get(
+    '/:table/:by',
+    [validateJWT],
+    findInCollection
 );
 
 module.exports = router;
