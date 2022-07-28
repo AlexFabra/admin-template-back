92c1cc976e1ba6a1d98212d2c5de28a4fb255b19
Alejandro <alfaseart@gmail.com>
2022-07-28 15:14:30 +0200

    need privileges to put or delete user

--- routes/users.js
+++ routes/users.js
@@ -5,29 +5,35 @@ const { getUsers, postUser, updateUser, deleteUser } = require('../controllers/u
 const { check } = require('express-validator');
 const { validateFields } = require('../middlewares/validate-fields');
 const { validateJWT } = require('../middlewares/validate-jwt');
+const { validateAdminRole } = require('../middlewares/validate-jwt');
+const { validateAdminRoleOrOwner } = require('../middlewares/validate-jwt');
 
 const router = Router();
 
-router.get('/', validateJWT ,getUsers);
+router.get('/', validateJWT, getUsers);
 router.post('/',
     [
         check('name', 'the name is required').not().isEmpty(),
         check('password', 'the password is required').not().isEmpty(),
-        check('email','the email is required').isEmail(),
+        check('email', 'the email is required').isEmail(),
         validateFields
     ], postUser);
 
 router.put('/:id',
-[
-    validateJWT,
-    check('name', 'the name is required').not().isEmpty(),
-    check('email','the email is required').isEmail(),
-    check('role','the role is required').not().isEmpty(),
-    validateFields
-],updateUser)
+    [
+        validateJWT,
+        validateAdminRoleOrOwner,
+        check('name', 'the name is required').not().isEmpty(),
+        check('email', 'the email is required').isEmail(),
+        check('role', 'the role is required').not().isEmpty(),
+        validateFields
+    ], updateUser)
 
 router.delete('/:id',
-validateJWT
-,deleteUser)
+    [
+        validateJWT,
+        validateAdminRole
+    ]
+    , deleteUser)
 
 module.exports = router;
